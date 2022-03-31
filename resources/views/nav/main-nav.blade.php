e44a51af14ee76b01ffe7e528af0f0f3946adf3b
recypher-zz <kniemerg001@me.com>
2022-03-30 21:05:01 -0700

    Created new nav bar system, still working on this however

---
+++ resources/views/nav/main-nav.blade.php
@@ -0,0 +1,4 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a href="#" id="navTitle" class="navbar-brand mx-4"><h4 class="display-4">Royal Roses</h4></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=""></button>
+</nav>
